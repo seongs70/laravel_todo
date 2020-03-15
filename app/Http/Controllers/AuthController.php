3334ed03dec4909e89cd84cda530eb602aa14028
ksh <seongs70@naver.com>
2020-03-15 10:25:25 +0900

    Controller modified

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,34 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
-    {
-        $http = new \GuzzleHttp\Client;
+    private $successStatus  =   200;
 
-        try {
-            $response = $http->post(config('services.passport.login_endpoint'), [
-                'form_params' => [
-                    'grant_type' => 'password',
-                    'client_id' => config('services.passport.client_id'),
-                    'client_secret' => config('services.passport.client_secret'),
-                    'email' => $request->email,
-                    'password' => $request->password,
-                    'scope' => '*',
-                ]
-            ]);
-            return $response->getBody();
-        } catch (\GuzzleHttp\Exception\BadResponseException $e) {
-            if ($e->getCode() === 400) {
-                return response()->json('Invalid Request. Please enter a username or a password.', $e->getCode());
-            } else if ($e->getCode() === 401) {
-                return response()->json('Your credentials are incorrect. Please try again', $e->getCode());
-            }
+    //----------------- [ Register user ] -------------------
+    public function registerUser(Request $request) {
 
-            return response()->json('Something went wrong on the server.', $e->getCode());
+        $validator  =   Validator::make($request->all(),
+            [
+                'name'              =>      'required|min:3',
+                'email'             =>      'required|email',
+                'password'          =>      'required|alpha_num|min:5',
+                'confirm_password'  =>      'required|same:password'
+            ]
+        );
+
+        if($validator->fails()) {
+            return response()->json(['Validation errors' => $validator->errors()]);
         }
+
+        $input              =       array(
+            'name'          =>          $request->name,
+            'email'         =>          $request->email,
+            'password'      =>          bcrypt($request->password),
+            'address'       =>          $request->address,
+            'city'          =>          $request->city
+        );
+
+        // check if email already registered
+        $user                   =       User::where('email', $request->email)->first();
+        if(!is_null($user)) {
+            $data['message']     =      "Sorry! this email is already registered";
+            return response()->json(['success' => false, 'status' => 'failed', 'data' => $data]);
+        }
+
+        // create and return data
+        $user                   =       User::create($input);
+        $success['message']     =       "You have registered successfully";
+
+        return response()->json( [ 'success' => true, 'user' => $user ] );
+    }
+
+    // -------------- [ User Login ] ------------------
+
+    public function userLogin(Request $request) {
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+
+            // getting auth user after auth login
+            $user = Auth::user();
+
+            $token                  =       $user->createToken('token')->accessToken;
+            $success['success']     =       true;
+            $success['message']     =       "Success! you are logged in successfully";
+//            $success['token']       =       $token;
+
+            return response()->json(['success' => $success,'access_token' => $token], $this->successStatus);
+        }
+
+        else {
+            return response()->json(['error'=>'Unauthorised'], 401);
+        }
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens->each(function ($token, $key) {
+            $token->delete();
+        });
+        return response()->json('Logged out successfully',200);
     }
 }
